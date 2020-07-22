51b456e2793e4fa04fd0455ff1b7876db1458281
veronica lim <veron_yan@yahoo.com.sg>
2020-07-22 14:11:42 +0800

    created server.js

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
 const express = require("express");
-const apiRoutes = require("./")
+const apiRoutes = require("./routes/apiRoutes.js");
+const htmlRoutes = require("./routes/htmlRoute.js");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+app.use("/api", apiRoutes);
+app.use("/html", htmlRoutes);
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
